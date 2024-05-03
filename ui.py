501d85c118e913d4000a1a4eece26dc5f8ab54aa
Arian Dannemann <arian.dannemann@gmail.com>
2024-05-03 19:08:31 +0200

    -new: mouse click event

--- ui.py
+++ ui.py
@@ -4,10 +4,14 @@ import board
 import piece
 
 root = None
+canvas = None
 
 title = "Icarus"
 white_color = "#9f90b0"
 black_color = "#7d4a8d"
+
+square_width = 80
+square_height = 80
 
 def init():
     """
@@ -15,9 +19,15 @@ def init():
     """
 
     global root
+    global canvas
 
     root = tk.Tk()
     root.title(title)
+
+    canvas = tk.Canvas(width=square_width*8, height=square_height*8)
+    canvas.pack()
+
+    root.bind("<Button 1>", click_square)
 
     update()
 
@@ -27,21 +37,46 @@ def update():
     """
 
     global root
+    global canvas
 
     for row in range(0, 8):
         for file in range(0, 8):
 
             color = white_color if (row + file) % 2 == 0 else black_color
-
-
             text = f"{board.get_color(7-row, file).name}\n{board.get_piece(7-row, file).name}"
 
-            if board.get_color(7-row, file) == piece.Color.NONE:
-                square = tk.Label(root, bg=color, width=10, height=4, borderwidth=0, relief="solid")
-            else:
-                square = tk.Label(root, text=text, bg=color, width=10, height=4, borderwidth=0, relief="solid")
+            x = file * square_width
+            y = row * square_height
 
-            square.grid(row=row, column=file)
+            canvas.create_rectangle((x, y), (x+square_height, y+square_width), fill=color)
+
+            if board.get_color(7-row, file) != piece.Color.NONE:
+                canvas.create_text(x + square_width / 2, y + square_height / 2, text=text, fill="black")
+
+def click_square(event_origin):
+    """
+    Handles mous click event
+    """
+
+    row, file = get_square()
+
+    piece = board.get_piece(row, file)
+    color = board.get_color(row, file)
+
+    print(f"Clicked {color.name} {piece.name}")
+
+def get_square():
+    """
+    Turns x and y of mouse click into row and file
+    """
+
+    x = root.winfo_pointerx() - root.winfo_rootx()
+    y = root.winfo_pointery() - root.winfo_rooty()
+
+    row = 7-int(y / square_height)
+    file = int(x / square_width)
+
+    return row, file
 
 def keep_alive():
     global root
