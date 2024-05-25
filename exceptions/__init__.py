cbda2fce19ba364371b070fa3c7bf9e8be2eec1f
Arian Dannemann <arian.dannemann@gmail.com>
2024-05-25 12:49:51 +0200

    -bugfix: local pep8 checks exceptions directory now

--- exceptions/__init__.py
+++ exceptions/__init__.py
@@ -2,6 +2,7 @@
 Al custom exceptions
 """
 
+
 class InconsistentState(Exception):
     """
     Thrown whenever a board reaches an inconsistent state
--- scripts/stylecheck.sh
+++ scripts/stylecheck.sh
@@ -1,5 +1,6 @@
 echo -:- PEP8 -:-
 pycodestyle --ignore=E501,W504,W503 *.py
+pycodestyle --ignore=E501,W504,W503 */*.py
 echo -:- Pylint -:-
 pylint *.py
 echo -:- MyPy -:-
